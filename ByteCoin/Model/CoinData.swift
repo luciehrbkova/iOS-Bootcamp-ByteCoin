af108dcabd084e23879935c35c2e630377ce60fa
luciehrbkova <50968340+luciehrbkova@users.noreply.github.com>
2022-03-19 13:20:32 +0000

    set up

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Lucie Hrbkova on 06/02/2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
---
+++ ByteCoin/Model/CoinModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by Lucie Hrbkova on 06/02/2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinModel {
+    let currentPrice: Double
+}
